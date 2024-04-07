440830d74662e71149cb79cc25961069c1ecd0f0
CKAJIA <128708356+CKAJIA@users.noreply.github.com>
2024-04-07 15:34:01 +0500

    rsg-composites

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version "adamant"
+games {"rdr3"}
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+shared_scripts {
+    "exports.js",
+    'config.lua',
+}
+
+client_scripts {
+	'client/client.lua',
+	'client/utilites.lua',
+	'client/deletedherbs.lua',
+	'client/dataview.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/server.lua'
+}
+
+exports {
+    'NativeCreateComposite',
+    'NativeGetCompositeEntities',
+	'StartCreateComposite',
+	'FindPicupCompositeAndCoords'
+}
